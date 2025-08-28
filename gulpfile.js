a2ca51e85b32351b20f7b89027372c39c9630909
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-08-28 14:16:29 -0300

    Teste Projeto 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,34 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require ('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
-function scripts(){
-    return gulp.src('./src/scripts/*.js')
+function scripts() {
+  return gulp.src('./src/scripts/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'));
 }
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
+  return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
-    return gulp.src('./src/images/**/*', {encoding: false})
+  return gulp.src('./src/images/**/*', {encoding: false})
     .pipe(imagemin())
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images,  scripts);
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+exports.build = gulp.series(scripts, styles, images);
+
+function watch() {
+  gulp.watch('./src/styles/*.scss', styles);
+  gulp.watch('./src/scripts/*.js', scripts);
+  gulp.watch('./src/images/**/*', images);
 }
+
+exports.watch = watch;
+
+exports.default = exports.build;
